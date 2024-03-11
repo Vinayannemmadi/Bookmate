b9e5e11b03668ecf1051cb360ed8021f817054b8
Vinaynemmadi <vinaynemmadi7@gmail.com>
2024-03-11 11:13:05 +0530

    modified build command

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use('/files', express.static('files'));
 
 app.use(express.static(path.join(__dirname,'/client/dist')));
 app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+  res.sendFile(path.join(__dirname,'client','build','index.html'));
 });
 
 app.listen(5000, () => console.log('Listening on port 5000...'));
